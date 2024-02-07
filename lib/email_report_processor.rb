0c54b53dfe6154414c7f5675bf4a011d65d688ec
Romain Tartière <romain@blogreen.org>
2024-02-07 09:11:59 -1000

    Rework MBox processing
    The previous code did not handled MBox with DOS line ending, and loaded
    the full mbox into memory for processing.  Rework the way we read mail
    from a mbox file, add some testing and rename the class to match the
    common naming for such files.

--- lib/email_report_processor.rb
+++ lib/email_report_processor.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require 'email_report_processor/mailbox'
 require 'email_report_processor/maildir'
+require 'email_report_processor/mbox'
 require 'email_report_processor/dmarc_rua'
 require 'email_report_processor/report_processor'
 require 'email_report_processor/tlsrpt_rua'
