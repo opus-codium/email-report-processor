e3e52b04200114bc7e676eb40aade63bcd8a2d69
Romain Tartière <romain@blogreen.org>
2024-02-09 09:53:15 -1000

    Test DMARC reports without DKIM auth_results
    Some reports do not include DKIM auth_results.  Trying to flatten these
    cause inconsistent document being generated, so make sure we obtain what
    we expect in this case.

--- spec/email_report_processor/dmarc_rua_spec.rb
+++ spec/email_report_processor/dmarc_rua_spec.rb
@@ -20,10 +20,22 @@ RSpec.describe EmailReportProcessor::DmarcRua do
   describe '#report' do
     let(:raw_report) { File.read('spec/fixtures/sample-reports/dmarc/google.com!example.com!1705363200!1705449599.xml') }
 
-    it 'sends reports' do
+    before do
       allow(processor).to receive(:send_report)
       processor.report(raw_report)
+    end
+
+    it 'sends reports' do
       expect(processor).to have_received(:send_report)
+    end
+
+    context 'without DKIM' do
+      let(:raw_report) { File.read('spec/fixtures/sample-reports/dmarc/enterprise.protection.outlook.com!blogreen.org!1706572800!1706659200.xml') }
+
+      it 'does not include DKIM auth_results' do
+        expect(processor).to have_received(:send_report)
+          .with(hash_including('feedback' => hash_including('record' => hash_including('auth_results' => hash_not_including('dkim')))))
+      end
     end
   end
 end
